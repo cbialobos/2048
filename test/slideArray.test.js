9c24cec75833764f9e68fce4801d5be458011b14
BIALOBOS, Christophe <c.bialobos@evs.com>
2018-12-18 22:08:18 +0100

    add tests

--- test/slideArray.test.js
+++ test/slideArray.test.js
@@ -25,4 +25,12 @@ describe("slideArray", () => {
     it("Should work on full array of different values and spaces with two sets of duplicate values", () => {
         expect(slideArray([2, 2, 4, 4])).toEqual([4, 8, null, null]);
     });
+
+    it("2_x_2_x --> 4_x_x_x", () => {
+        expect(slideArray([2, null, 2, null])).toEqual([4, null, null, null]);
+    });
+
+    it("x_2_x_2 --> 4_x_x_x", () => {
+        expect(slideArray([null, 2, null, 2])).toEqual([4, null, null, null]);
+    });
 });
